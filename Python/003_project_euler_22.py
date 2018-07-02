7f40988e9877ede4ae9969c4aebc062425ac30b8
Yashas Lokesh <yashas.lokesh@gmail.com>
2018-07-02 10:44:28 -0400

    Didn't have WiFi for my laptop last night, so I'm adding the solution for ProjectEuler.net's problem #22. This module uses csv. I changed the type of the names.txt file I got from ProjectEuler.net to .csv instead, so that I could get some practice with using the csv module.

---
+++ Python/003_project_euler_22.py
@@ -0,0 +1,23 @@
+import csv
+
+names = list()
+
+with open("supporting_files/project_euler_p022_names.csv","r") as names_file:
+    reader = csv.reader(names_file)
+    for row in reader:
+        for col in row:
+            names.append(col)
+
+names.sort()
+
+def calcNameScore( name : str, position : int) -> int:
+    value = 0
+    for letter in list(name):
+        value += ord(letter) - 64
+    return value * position
+
+sum = 0
+for name in names:
+    sum += calcNameScore(name, names.index(name) + 1)
+
+print(sum)
