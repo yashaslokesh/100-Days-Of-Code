8c2c9ada12c4de698fe373e47138de0fc3d8a420
Yashas Lokesh <yashas.lokesh@gmail.com>
2018-07-09 22:48:50 -0400

    Added timing functionality and a final count + calculation time display at the end

--- Python/d010_prime_sieve/d010_prime_sieve.py
+++ Python/d010_prime_sieve/d010_prime_sieve.py
@@ -1,12 +1,17 @@
 import math
+import time
 
 number = int(input("Enter a number, and all primes below it shall be found: "))
 
 numbers = list(range(2, number))
 
+start = time.time()
 for i in range(2, math.floor(math.sqrt(number))):
     for num in range(i**2, number, i):
         if num in numbers:
             numbers.remove(num)
+end = time.time()
 
 print(numbers)
+print(f"\nNumber of primes below {number}: {len(numbers)}")
+print(f"The primes were calculated in {end - start} seconds")
