1d9abc5334e160d40223a4ccd16a36133a6e976e
Yashas Lokesh <yashas.lokesh@gmail.com>
2018-07-15 21:43:39 -0400

    Created a script that will accept text and a re as input and print out all matches

---
+++ Python/d017_using_re/d017_using_re.py
@@ -0,0 +1,17 @@
+
+import re
+
+def regex_finder():
+    text = input("Input the text you would like to search through with a regular expression: ")
+    pattern = re.compile(input("Enter the regular expression you would like to use to query the text: "))
+    matches = pattern.search(text)
+    if not matches.groups():
+        print("No matches found")
+    else:
+        print(f"{len(matches.groups())} matches found")
+        for match in matches.groups():
+            print(f"Match found: {match}")
+    
+if __name__ == "__main__":
+    regex_finder()
+
