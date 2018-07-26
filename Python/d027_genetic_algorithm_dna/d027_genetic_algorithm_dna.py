becb5f1605ca19c3040cdf1f6d9116c250136516
Yashas Lokesh <yashas.lokesh@gmail.com>
2018-07-25 23:19:08 -0400

    Created DNA class
    Created DNA class based on The Nature of Code at https://natureofcode.com/book/chapter-9-the-evolution-of-code/.

---
+++ Python/d027_genetic_algorithm_dna/d027_genetic_algorithm_dna.py
@@ -0,0 +1,27 @@
+import random
+
+class DNA():
+
+    def __init__(self, data_length):
+        self.chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !,."
+        self.genes = [letter for letter in random.choices(self.chars, k=data_length)]
+    
+    def fitness(self, target):
+        result = 0
+        for i in range(len(self.genes)):
+            result += 1 if self.genes[i] == target[i] else 0
+        return result / len(target)
+
+    def crossover(self, partner):
+        child = DNA(len(self.genes))
+
+        mid = int(random.randrange(len(self.genes)))
+
+        for gene in range(len(self.genes)):
+            child.genes[gene] = self.genes[gene] if gene > mid else partner.genes[gene]
+        return child
+
+    def mutate(self, rate):
+        for gene in range(len(self.genes)):
+            if random.randrange(2) < rate:
+                self.genes[gene] = random.choice(self.chars)
