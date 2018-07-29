ab5f949fd13356317b054cb77d6b0d6b1400abfc
Yashas Lokesh <yashas.lokesh@gmail.com>
2018-07-29 13:03:18 -0400

    Added list comprehensions for DNA class

--- Python/d029_first_genetic_algorithm/dna.py
+++ Python/d029_first_genetic_algorithm/dna.py
@@ -11,10 +11,9 @@ class DNA():
         return "".join(self.genes)
     
     def calc_fitness(self, target):
-        result = 0
-        for i in range(len(self.genes)):
-            result += 1 if self.genes[i] == target[i] else 0
-        self.fitness = result / len(target)
+        raw_fitness = sum([1 if self.genes[i] == target[i] else 0
+                           for i,_ in enumerate(self.genes)])
+        self.fitness = raw_fitness / len(target)
     
     @property
     def fitness(self):
@@ -25,15 +24,14 @@ class DNA():
         self._fitness = value
 
     def crossover(self, partner):
-
         child = DNA(len(self.genes))
         mid = int(random.randrange(len(self.genes)))
 
-        for i,gene in enumerate(self.genes):
-            child.genes[i] = gene if i > mid else partner.genes[i]
+        child.genes = [gene if i > mid else partner.genes[i]
+                       for i, gene in enumerate(self.genes)]
         return child
 
     def mutate(self, rate):
-        for i,_ in enumerate(self.genes):
-            if random.random() < rate:
-                self.genes[i] = random.choice(self.chars)
+        self.genes = [random.choice(self.chars) if random.random() < rate else gene
+                      for gene in self.genes]
+        return self
