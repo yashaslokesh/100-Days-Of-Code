0d58e0380460f56343be5a533988a6d744bc9c6c
Yashas Lokesh <yashas.lokesh@gmail.com>
2018-08-11 17:03:04 -0400

    Added google translation api code

---
+++ Python/d042_translation/translation.py
@@ -0,0 +1,37 @@
+from google.cloud import translate
+
+def translate_phrase(source, target, phrase):
+    client = translate.Client()
+
+    source_language = [
+        entry['language'] for entry in client.get_languages() 
+        if entry['name'].lower() == source.strip().lower()
+    ]
+
+    target_language = [
+        entry['language'] for entry in client.get_languages() 
+        if entry['name'].lower() == target.strip().lower()
+    ]
+    print()
+
+    if source.strip():
+        result = client.translate(phrase, source_language=source_language[0], 
+                 target_language=target_language[0])
+        print(f'{source} was used as the source language.\n'
+              f'Translating "{phrase}" to {target}:\n{result["translatedText"]}')
+    else:
+        result = client.translate(phrase, target_language=target_language[0])
+
+        print(f'The language {result["detectedSourceLanguage"]} was detected.\n'
+              f'Translating "{phrase}" to {target}:\n{result["translatedText"]}')
+    
+    print()
+
+def main():
+    source = input("Enter the language your phrase is in, or leave blank to infer language: ")
+    target = input("Enter target language for translation: ")
+    phrase = input("Enter the phrase you would like translated: ")
+    translate_phrase(source, target, phrase)
+
+if __name__ == '__main__':
+    main()
