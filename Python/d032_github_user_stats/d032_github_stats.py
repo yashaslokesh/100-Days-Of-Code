3c2014f7975177d26ece357239d703080c28b887
Yashas Lokesh <yashas.lokesh@gmail.com>
2018-08-11 16:30:13 -0400

    Added github statistics code

--- Python/d032_github_user_stats/d032_github_stats.py
+++ Python/d032_github_user_stats/d032_github_stats.py
@@ -1,23 +1,36 @@
+""" This module is used to retrieve and display a GitHub user's public repos info.
 
+"""
+
+import datetime
 import requests
 import json
-
-"""
-    Only access public repositories. The requests module is used to make GitHub API calls, 
-    from which all information will be taken.
-"""
 
 def retrieve(name):
     url = f"https://api.github.com/users/{name}/repos"
     response = requests.get(url)
-    return json.loads(response.text)
+    data = json.loads(response.text)
+    with open("d032_github_user_stats/data.json","w") as f:
+        f.write(json.dumps(data, indent=2))
+    return data
 
 def process(data):
-    pass
+    repo_results = [
+        (f'Repo: {repo["name"]}\nDescription: {repo["description"]}\nLink: {repo["html_url"]}\n'
+        f'Stars: {repo["stargazers_count"]}\nForks: {repo["forks_count"]}\n')
+        for repo in data
+    ]
+
+    return '\n'.join(repo_results)
 
 def main():
     username = input("Enter your GitHub Username: ")
-    data = retrieve(username)
+    data = retrieve(name=username)
+    result = process(data)
+
+    today = datetime.datetime.today().strftime("%m-%d-%Y")
+    with open(f"github-repos-{today}.txt","w") as f:
+        f.write(result)
 
 
 if __name__ == '__main__':
